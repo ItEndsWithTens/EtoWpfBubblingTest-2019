ab9556d844096eebce2520aa62db1619327bfa8d
Rob Martens <robert.martens@gmail.com>
2019-07-16 16:41:05 -0400

    Add .editorconfig

--- EtoWpfBubblingTest/MainForm.cs
+++ EtoWpfBubblingTest/MainForm.cs
@@ -2,28 +2,28 @@
 
 namespace EtoWpfBubblingTest
 {
-    public partial class MainForm
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+	public partial class MainForm
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
 
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            base.OnKeyDown(e);
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                System.Diagnostics.Debugger.Break();
-            }
-        }
-        protected override void OnMouseDown(MouseEventArgs e)
-        {
-            base.OnMouseDown(e);
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                System.Diagnostics.Debugger.Break();
-            }
-        }
-    }
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			base.OnKeyDown(e);
+			if (System.Diagnostics.Debugger.IsAttached)
+			{
+				System.Diagnostics.Debugger.Break();
+			}
+		}
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			base.OnMouseDown(e);
+			if (System.Diagnostics.Debugger.IsAttached)
+			{
+				System.Diagnostics.Debugger.Break();
+			}
+		}
+	}
 }
--- EtoWpfBubblingTest/MainForm.eto.cs
+++ EtoWpfBubblingTest/MainForm.eto.cs
@@ -4,74 +4,74 @@ using System;
 
 namespace EtoWpfBubblingTest
 {
-    partial class MainForm : Form
-    {
-        private RadioButton rdoCheckBox;
-        private RadioButton rdoPanel;
-        private RadioButton rdoWindowsFormsHost;
+	partial class MainForm : Form
+	{
+		private RadioButton rdoCheckBox;
+		private RadioButton rdoPanel;
+		private RadioButton rdoWindowsFormsHost;
 
-        private PixelLayout pxlViewport = new PixelLayout { BackgroundColor = Colors.Green, Size = new Size(100, 100) };
+		private PixelLayout pxlViewport = new PixelLayout { BackgroundColor = Colors.Green, Size = new Size(100, 100) };
 
-        private NodeCheckBox nodeCheckBox = new NodeCheckBox { Size = new Size(100, 100), BackgroundColor = Colors.Cyan };
-        private NodePanel nodePanel = new NodePanel { Size = new Size(100, 100), BackgroundColor = Colors.Purple };
-        private NodeWindowsFormsHost nodeWindowsFormsHost = new NodeWindowsFormsHost { Size = new Size(100, 100), BackgroundColor = Colors.White };
+		private NodeCheckBox nodeCheckBox = new NodeCheckBox { Size = new Size(100, 100), BackgroundColor = Colors.Cyan };
+		private NodePanel nodePanel = new NodePanel { Size = new Size(100, 100), BackgroundColor = Colors.Purple };
+		private NodeWindowsFormsHost nodeWindowsFormsHost = new NodeWindowsFormsHost { Size = new Size(100, 100), BackgroundColor = Colors.White };
 
-        void InitializeComponent()
-        {
-            Title = $"EtoWpfBubblingTest - Platform: {Eto.Platform.Instance.ID}";
-            ClientSize = new Size(400, 350);
+		void InitializeComponent()
+		{
+			Title = $"EtoWpfBubblingTest - Platform: {Eto.Platform.Instance.ID}";
+			ClientSize = new Size(400, 350);
 
-            rdoWindowsFormsHost = new RadioButton { Text = "NodeWindowsFormsHost" };
-            rdoPanel = new RadioButton(rdoWindowsFormsHost) { Text = "NodePanel" };
-            rdoCheckBox = new RadioButton(rdoWindowsFormsHost) { Text = "NodeCheckBox" };
+			rdoWindowsFormsHost = new RadioButton { Text = "NodeWindowsFormsHost" };
+			rdoPanel = new RadioButton(rdoWindowsFormsHost) { Text = "NodePanel" };
+			rdoCheckBox = new RadioButton(rdoWindowsFormsHost) { Text = "NodeCheckBox" };
 
-            Content = new TableLayout()
-            {
-                BackgroundColor = Colors.Red,
-                Rows = 
-                {
-                    new TableRow()
-                    {
-                        Cells =
-                        {
-                            new StackLayout
-                            {
-                                Items =
-                                {
-                                    rdoCheckBox,
-                                    rdoPanel,
-                                    rdoWindowsFormsHost
-                                }
-                            },
-                            pxlViewport
-                        }
-                    }
-                }
-            };
+			Content = new TableLayout()
+			{
+				BackgroundColor = Colors.Red,
+				Rows =
+				{
+					new TableRow()
+					{
+						Cells =
+						{
+							new StackLayout
+							{
+								Items =
+								{
+									rdoCheckBox,
+									rdoPanel,
+									rdoWindowsFormsHost
+								}
+							},
+							pxlViewport
+						}
+					}
+				}
+			};
 
-            rdoCheckBox.CheckedChanged += ChangeNodeType;
-            rdoPanel.CheckedChanged += ChangeNodeType;
-            rdoWindowsFormsHost.CheckedChanged += ChangeNodeType;
+			rdoCheckBox.CheckedChanged += ChangeNodeType;
+			rdoPanel.CheckedChanged += ChangeNodeType;
+			rdoWindowsFormsHost.CheckedChanged += ChangeNodeType;
 
-            rdoWindowsFormsHost.Checked = true;
-        }
+			rdoWindowsFormsHost.Checked = true;
+		}
 
-        private void ChangeNodeType(object sender, EventArgs e)
-        {
-            pxlViewport.RemoveAll();
+		private void ChangeNodeType(object sender, EventArgs e)
+		{
+			pxlViewport.RemoveAll();
 
-            if (rdoCheckBox.Checked)
-            {
-                pxlViewport.Add(nodeCheckBox, 0, 0);
-            }
-            else if (rdoPanel.Checked)
-            {
-                pxlViewport.Add(nodePanel, 0, 0);
-            }
-            else if (rdoWindowsFormsHost.Checked)
-            {
-                pxlViewport.Add(nodeWindowsFormsHost, 0, 0);
-            }
-        }
-    }
+			if (rdoCheckBox.Checked)
+			{
+				pxlViewport.Add(nodeCheckBox, 0, 0);
+			}
+			else if (rdoPanel.Checked)
+			{
+				pxlViewport.Add(nodePanel, 0, 0);
+			}
+			else if (rdoWindowsFormsHost.Checked)
+			{
+				pxlViewport.Add(nodeWindowsFormsHost, 0, 0);
+			}
+		}
+	}
 }
